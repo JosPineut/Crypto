4954013a34ec2a144a83e633c290e78ebbab94cb
Järn <38694005+JosPineut@users.noreply.github.com>
2018-12-13 14:13:51 +0100

    bytes omgezet

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,8 @@ public class Main {
         Person person1 = new Person("Brian Mariman","Warehem","0474654872");
 
         Person person1fake = new Person("Brion Mariman","Warehem","0474654872");
+
+        byte[] person1bytes = person1.getBytes();
+        byte[] person1fakebytes = person1fake.getBytes();
     }
 }
