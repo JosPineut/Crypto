49975547dd7d1533ec936e8c4514dc8d768eca91
Järn <38694005+JosPineut@users.noreply.github.com>
2018-12-13 14:11:50 +0100

    Personfake 1 aangemaakt

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         Person person1 = new Person("Brian Mariman","Warehem","0474654872");
+
+        Person person1fake = new Person("Brion Mariman","Warehem","0474654872");
     }
 }
