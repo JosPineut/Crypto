3d0663f84cce2a5a1c63a3b87599bf5427d49c57
Järn <38694005+JosPineut@users.noreply.github.com>
2018-12-13 14:20:33 +0100

    bytes omgezet

--- src/Main.java
+++ src/Main.java
@@ -10,14 +10,28 @@ public class Main {
         Person person1fake = new Person("Brion Mariman","Warehem","0474654872");
 
         byte[] person1bytes = person1.getBytes();
-        byte[] personBytes = person2.getBytes();
+        byte[] person2Bytes = person2.getBytes();
         byte[] person1fakebytes = person1fake.getBytes();
 
         try {
             MessageDigest sha = MessageDigest.getInstance("SHA-256");
+
+            sha.update(person1bytes);
+            //sha.update(person2Bytes);
+
+            sha.update(person1fakebytes);
+
+            System.out.println(new String(person1bytes));
+            //System.out.println(new String(person2Bytes));
+            System.out.println("andere:");
+            System.out.println(new String(person1fakebytes));
+
+
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
+
+
 
     }
 }
